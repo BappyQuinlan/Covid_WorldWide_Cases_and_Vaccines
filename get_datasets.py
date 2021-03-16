50cd619200e9e3404bbc67d893ecaa3b63ad9702
Barry Quinlan <bappyquinlan@gmail.com>
2021-03-16 13:45:56 +0000

    Added Libraries, matplotlib, kaggle, seaborn and pandas.  Added first class

--- get_datasets.py
+++ get_datasets.py
@@ -1,8 +1,17 @@
 class get_dataset():
     from kaggle.api.kaggle_api_extended import KaggleApi
+    import zipfile
 
     api = KaggleApi()
     api.authenticate()
 
+    f = 'country_vaccinations.csv'
+    dir='./'
+
     # Signature: dataset_download_file(dataset, file_name, path=None, force=False, quiet=True)
-    api.dataset_download_file('gpreda/covid-world-vaccination-progress', 'country_vaccinations.csv')
+    api.dataset_download_file('gpreda/covid-world-vaccination-progress', f)
+
+    api.dataset_download_file('imdevskp/corona-virus-report', 'country_wise_latest.csv')
+
+    with zipfile.ZipFile(f+'.zip', 'r') as zip_ref:
+        zip_ref.extractall(dir)
